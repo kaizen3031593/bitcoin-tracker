0258d197b435267cd3a12dbae764af9f83ac0726
Kaizen Conroy <kcc2@williams.edu>
2021-03-28 01:11:19 -0400

    add dynamodb/api-gateway/lambda

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { AppStack } from '../lib/app-stack';
 
 const app = new cdk.App();
 new InfrastructureStack(app, 'InfrastructureStack', {
@@ -18,4 +19,8 @@ new InfrastructureStack(app, 'InfrastructureStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+new AppStack(app, 'AppStack',{
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
---
+++ infrastructure/lib/app-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hitcounter';
+
+
+export class AppStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // defines an AWS Lambda resource
+        const hello = new lambda.Function(this, 'HelloHandler', {
+            runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+            code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+            handler: 'hello.handler'                // file is "hello", function is "handler"
+        });
+
+        const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+            downstream: hello
+        });
+      
+        // defines an API Gateway REST API resource backed by our "hello" function.
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: helloWithCounter.handler
+        });
+
+        // const table = new dynamodb.Table(this, 'Hits', {
+        //     tableName: 'subscribers',
+        //     partitionKey: { name: 'date', type: dynamodb.AttributeType.NUMBER }
+        // });
+
+        // table.grantReadWriteData(this.handler);
+    }
+}
