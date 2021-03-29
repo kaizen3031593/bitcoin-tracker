3ab902e29d18c3cc80eba5493def0f2ad8cef562
Kaizen Conroy <kcc2@williams.edu>
2021-03-29 12:18:44 -0400

    update lambda headers to allow cors

--- infrastructure/lambda/hello.js
+++ infrastructure/lambda/hello.js
@@ -2,7 +2,12 @@ exports.handler = async function(event) {
   console.log("request:", JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
+    headers: { 
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Headers" : "*",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
     body: `Hello, CDK! You've hit ${event.path}\n`
   };
 };
