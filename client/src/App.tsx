b224353228c76db0b7d322a04f3b60605920a61d
Kaizen Conroy <kcc2@williams.edu>
2021-03-29 18:48:47 -0400

    button calls api now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,80 +1,52 @@
 import React, { useState } from 'react';
-import axios, { CancelTokenSource } from 'axios';
+import axios from 'axios';
 import './App.css';
 
 interface IPost {
-  body: string;
+  id: number;
+  data: string;
 }
 
 const defaultPosts: IPost[] = [];
+var num = 0;
+
 
 const App: React.FC = () => {
   const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = useState(
     defaultPosts
   );
 
-  const [loading, setLoading]: [
+  const [refresh, setRefresh]: [
     boolean,
-    (loading: boolean) => void
-  ] = useState<boolean>(true);
+    (refresh: boolean) => void
+  ] = React.useState<boolean>(false);
 
-  const [error, setError]: [string, (error: string) => void] = useState(
-    ''
-  );
-  const cancelToken = axios.CancelToken;
-  const [cancelTokenSource]: [
-    CancelTokenSource,
-    (cancelTokenSource: CancelTokenSource) => void
-  ] = useState(cancelToken.source());
-
-  const handleCancelClick = () => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel('User cancelled operation');
-    }
+  const callAPIClick = () => {
+    setRefresh(false);
+    axios.get(`https://ln7kvmlhug.execute-api.us-east-1.amazonaws.com/prod/${num}`)
+    .then((response) => {
+      console.log(response);
+      defaultPosts.push({id: num++, data: response.data});
+      setPosts(defaultPosts);
+      setRefresh(true);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   };
-
-  axios.get('https://ln7kvmlhug.execute-api.us-east-1.amazonaws.com/prod/')
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
 
   return (
     <div className="App">
-      {loading && <button onClick={handleCancelClick}>Cancel</button>}
+      {<button onClick={callAPIClick}>Run</button>}
       <ul className="posts">
-        {posts.map((post) => (
-          <li>
-            <p>{post.body}</p>
+        {refresh && posts.map((post) => (
+          <li key={post.id}>
+            <p>{post.data}</p>
           </li>
         ))}
       </ul>
-      {error && <p className="error">{error}</p>}
     </div>
   );
 };
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
