1c32af89bff84ace055407d5c56534188955408e
Kaizen Conroy <kcc2@williams.edu>
2021-03-29 12:20:50 -0400

    app makes get request to api gateway

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios, { CancelTokenSource } from 'axios';
 import './App.css';
 
 interface IPost {
-  userId: number;
-  id?: number;
-  title: string;
   body: string;
 }
 
 const defaultPosts: IPost[] = [];
 
-const App: React.SFC = () => {
-  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+const App: React.FC = () => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = useState(
     defaultPosts
   );
 
   const [loading, setLoading]: [
     boolean,
     (loading: boolean) => void
-  ] = React.useState<boolean>(true);
+  ] = useState<boolean>(true);
 
-  const [error, setError]: [string, (error: string) => void] = React.useState(
+  const [error, setError]: [string, (error: string) => void] = useState(
     ''
   );
   const cancelToken = axios.CancelToken;
   const [cancelTokenSource]: [
     CancelTokenSource,
     (cancelTokenSource: CancelTokenSource) => void
-  ] = React.useState(cancelToken.source());
+  ] = useState(cancelToken.source());
 
   const handleCancelClick = () => {
     if (cancelTokenSource) {
@@ -36,38 +33,20 @@ const App: React.SFC = () => {
     }
   };
 
-  React.useEffect(() => {
-    axios
-      .get<IPost[]>('https://jsonplaceholder.typicode.com/posts', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        timeout: 5000,
-      })
-      .then((response) => {
-        setPosts(response.data);
-        setLoading(false);
-      })
-      .catch((ex) => {
-        const err = axios.isCancel(ex)
-          ? 'Request cancelled'
-          : ex.code === 'ECONNABORTED'
-          ? 'A timeout has occurred'
-          : ex.response.status === 404
-          ? 'Resource not found'
-          : 'An unexpected error has occurred';
-        setError(err);
-        setLoading(false);
-      });
-      cancelTokenSource.cancel("User cancelled operation");
-  }, [cancelTokenSource]);
+  axios.get('https://ln7kvmlhug.execute-api.us-east-1.amazonaws.com/prod/')
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
   return (
     <div className="App">
       {loading && <button onClick={handleCancelClick}>Cancel</button>}
       <ul className="posts">
         {posts.map((post) => (
-          <li key={post.id}>
-            <h3>{post.title}</h3>
+          <li>
             <p>{post.body}</p>
           </li>
         ))}
