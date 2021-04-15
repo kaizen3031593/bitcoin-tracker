22ad282d21554bac4657357946be3beb0ac05ebf
Kaizen Conroy <kcc2@williams.edu>
2021-04-15 17:49:46 -0400

    add test file

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as ReactDOM from 'react-dom'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -7,3 +8,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
